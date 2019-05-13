da13bbe75b9080374a34cf63de49e7c453212441
Alword <anzer987@yandex.ru>
2019-05-13 20:16:00 +0300

    add interaction

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SimpleCalculator
 {
     public partial class MainWindow : Window
     {
         private double lastNumber, result;
+        private SelectedOperator selectedOperator;
 
         public MainWindow()
         {
@@ -21,6 +22,27 @@ namespace SimpleCalculator
 
         private void EqualsButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!double.TryParse(ResultLabel.Content.ToString(), out double tempNumber)) return;
+
+            switch (selectedOperator)
+            {
+                case SelectedOperator.Multiplication:
+                    result = SimpleMath.Multiplication(lastNumber, tempNumber);
+                    break;
+                case SelectedOperator.Subtraction:
+                    result = SimpleMath.Subtraction(lastNumber, tempNumber);
+                    break;
+                case SelectedOperator.Addition:
+                    result = SimpleMath.Addition(lastNumber, tempNumber);
+                    break;
+                case SelectedOperator.Division:
+                    result = SimpleMath.Division(lastNumber, tempNumber);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            ResultLabel.Content = result;
 
         }
 
@@ -45,16 +67,68 @@ namespace SimpleCalculator
             ResultLabel.Content = "0";
         }
 
-        private void SevenButton_OnClick(object sender, RoutedEventArgs e)
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
         {
-            if (ResultLabel.Content.Equals("0"))
+            if (!double.TryParse(ResultLabel.Content.ToString(), out lastNumber)) return;
+
+            if (sender.Equals(MultiplyButton)) selectedOperator = SelectedOperator.Multiplication;
+            if (sender.Equals(DivideButton)) selectedOperator = SelectedOperator.Division;
+            if (sender.Equals(PlusButton)) selectedOperator = SelectedOperator.Addition;
+            if (sender.Equals(MinusButton)) selectedOperator = SelectedOperator.Subtraction;
+
+            ResultLabel.Content = "0";
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            string content = (sender as Button)?.Content.ToString();
+            if (content == null) return;
+            int selectedValue = int.Parse(content); ;
+
+            if (ResultLabel.Content.ToString().Equals("0"))
             {
-                ResultLabel.Content = "7";
+                ResultLabel.Content = $"{selectedValue}";
             }
             else
             {
-                ResultLabel.Content += "7";
+                ResultLabel.Content += $"{selectedValue}";
             }
+        }
+
+        private void DotButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!ResultLabel.Content.ToString().Contains("."))
+            {
+                ResultLabel.Content = $"{ResultLabel.Content}.";
+            }
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Division(double a, double b)
+        {
+            return a / b;
         }
     }
 }
