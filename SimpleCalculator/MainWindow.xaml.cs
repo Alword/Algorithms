70ddf82ec51f4664efb06ea2235408d0bf77e787
Alword <anzer987@yandex.ru>
2019-05-13 19:05:27 +0300

    Add Invert features

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -1,28 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleCalculator
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ResultLabel.Content = "12345";
+            AcButton.Click += AcButton_Click;
+            ChangeButton.Click += ChangeButton_Click;
+            PercentButton.Click += PercentButton_Click;
+            EqualsButton.Click += EqualsButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(ResultLabel.Content.ToString(), out lastNumber)) return;
+
+            lastNumber /= 100;
+            ResultLabel.Content = lastNumber;
+        }
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!double.TryParse(ResultLabel.Content.ToString(), out lastNumber)) return;
+
+            lastNumber *= -1;
+            ResultLabel.Content = lastNumber;
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = "0";
+        }
+
+        private void SevenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (ResultLabel.Content.Equals("0"))
+            {
+                ResultLabel.Content = "7";
+            }
+            else
+            {
+                ResultLabel.Content += "7";
+            }
         }
     }
 }
