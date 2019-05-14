ce3bace1ff73c54859fc7c06fed33b2213dfebec
Alword <anzer987@yandex.ru>
2019-05-14 22:49:55 +0300

    add createcontactwindow

---
+++ DesktopContactsApp/CreateContactWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace DesktopContactsApp
+{
+    // ReSharper disable once RedundantExtendsListEntry
+    public partial class CreateContactWindow : Window
+    {
+        public CreateContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
