54d87f584b067d96f9af5708a36eb066187476fa
Alword <anzer987@yandex.ru>
2019-05-20 17:26:16 +0300

    create database connection

--- DesktopContactsApp/CreateContactWindow.xaml.cs
+++ DesktopContactsApp/CreateContactWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Shapes;
+using DesktopContactsApp.Classes;
+using SQLite;
 
 namespace DesktopContactsApp
 {
@@ -13,6 +16,20 @@ namespace DesktopContactsApp
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            var contact = new Contact()
+            {
+                Name = NameTextBox.Text,
+                Email = EmailTextBox.Text,
+                Phone = PhoneNameTextBox.Text
+            };
+            var databaseName = "Contact.db";
+            string databaseFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string path = System.IO.Path.Combine(databaseFolder, databaseName);
+
+            SQLiteConnection connection = new SQLiteConnection(path);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            connection.Close();
             Close();
         }
     }
